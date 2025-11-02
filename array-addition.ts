b51b7597a50984c9c840bfcfcd41152f4e4abf4c
Julian M <julianemartinez1995@gmail.com>
2025-11-01 21:29:03 -0400

    Initial commit

---
+++ array-addition.ts
@@ -0,0 +1,7 @@
+// Have the function arrayAddition(arr) take the array of numbers stored in arr and return true if any combination of numbers in the array (excluding the largest number) can add up exactly to the largest number in the array.
+// Otherwise return false. The array will not be empty, will not contain duplicates, and may contain negative numbers.
+
+function arrayAddition(arr: number[]): boolean {
+  return true;
+}
+console.log(arrayAddition([1, 2, 3, 4]));
