a3329d7c1a8df701d91c96284cf18f9cc981e690
Julian M <julianemartinez1995@gmail.com>
2025-11-02 20:13:41 -0500

    solution

--- array-addition.ts
+++ array-addition.ts
@@ -2,6 +2,17 @@
 // Otherwise return false. The array will not be empty, will not contain duplicates, and may contain negative numbers.
 
 function arrayAddition(arr: number[]): boolean {
-  return true;
+  let seen = new Set<number>();
+
+  const max = Math.max(...arr);
+  for (const num of arr) {
+    if (seen.has(max - num)) {
+      return true;
+    }
+    seen.add(num);
+    console.log(seen);
+  }
+
+  return false;
 }
 console.log(arrayAddition([1, 2, 3, 4]));
