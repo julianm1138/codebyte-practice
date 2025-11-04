7b02d98ff09560726ec6ee64ab96c38ba4b8f0b5
Julian M <julianemartinez1995@gmail.com>
2025-11-03 21:20:43 -0500

    Initial commit

---
+++ bracket-matcher.ts
@@ -0,0 +1,2 @@
+// Have the function bracketMatcher(str) take the str parameter being passed and return 1 if the parentheses are correctly matched and each one is accounted for.
+// Otherwise return 0. The string will not be empty and may contain non-parenthesis characters.
