4f695fba115ebf90ecf65e6f996d37bb7aa9454d
Julian M <julianemartinez1995@gmail.com>
2025-11-04 11:55:55 -0500

    solution

--- bracket-matcher.ts
+++ bracket-matcher.ts
@@ -1,2 +1,25 @@
 // Have the function bracketMatcher(str) take the str parameter being passed and return 1 if the parentheses are correctly matched and each one is accounted for.
 // Otherwise return 0. The string will not be empty and may contain non-parenthesis characters.
+//()
+function bracketMatcher(str: string): number {
+  let stack: string[] = [];
+
+  for (const char of str) {
+    if (char === "(") {
+      stack.push(char);
+    } else if (char === ")") {
+      if (stack.length === 0) {
+        return 0;
+      }
+    }
+    stack.pop();
+  }
+
+  if (stack.length > 0) {
+    return 0;
+  } else {
+    return 1;
+  }
+}
+
+console.log(bracketMatcher("(())"));
